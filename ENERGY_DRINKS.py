fae502eed05f17e0ebc221ac018c254a302c5dff
dechtel <muhammad.aljuburi2007@gmail.com>
2026-09-18 01:45:47 +0300

    Add files via upload

---
+++ ENERGY_DRINKS.py
@@ -0,0 +1,87 @@
+
+import json as js
+
+# reading drinks list
+d ={}
+with open("ENERGY_DRINKS.json",'r') as file :
+  try:
+    d = js.load(file)
+  except js.JSONDecodeError :
+    print("List is Empty")
+# reading text words that are separated by a space
+def words(TXT) :
+  word = ""
+  list_words= []
+  TEXT = TXT.strip()
+  TEXT += " "
+
+  for i in  TEXT :
+    if i == " " and word != "":
+      list_words.append(word)
+      word = ""
+    else :
+      word += i
+  return list_words
+
+
+while True :
+  # data analyzing main variables
+  command = str(input("–@– "))
+  commands= words(command)
+  length = len(commands)
+  
+  #print(command,"::",commands,"::",length) # test
+
+  if length > 2 :
+    print("A lot of Words")
+  
+  elif length > 1 and commands[0] == "list" :
+    print(d[commands[1].capitalize()])
+  
+  
+  elif length == 1 and commands[0] == "new":
+    NAME = str(input("drink name : ")).capitalize()
+    try :
+      if NAME in d :
+        print(f"{NAME} drink in the list")
+        print("by continueing you're updating the drink ")
+        if str(input("q to quit enter to resume : ")) == "q" :
+          exit()  # noqa: PLR1722
+      
+    
+      
+      taurine = float(input("Taurine Per 100mL : "))
+      inositol = float(input("Inositol Per 100mL : ")) 
+      caffeine = float(input("Caffeine Per 100mL : "))
+      total_size = int(input("Total Can Size : "))
+    
+    except KeyboardInterrupt :
+      exit()  # noqa: PLR1722
+    
+    except ValueError :
+      print("Enter Valid Values")
+    
+  elif length == 1 and commands[0] in ["q","Q","Quit","quit","QUIT"] :
+    print("We are QUITTING")
+    exit()  # noqa: PLR1722
+  
+  elif length == 1 and commands[0] == "save" :
+    with open("ENERGY_DRINKS.json","w") as file :
+      try:
+        d[NAME]={"Taurine":taurine,"Inositol":inositol,"Caffeine" : caffeine,"Total Size":total_size}
+      except NameError :
+        print("saving")
+      print("saving")
+      js.dump(d,file,indent=2)
+  
+  elif length == 2 and commands[0] in ["del","delete","remove","rm"] :
+    if commands[1].capitalize() in d :
+      d[commands[1].capitalize()] = {}
+    else :
+      print(f"{commands[1].capitalize()} Is NOT In The List")
+  elif length == 1 and commands[0] == "backup":
+    with open("BACKUP.json","w") as BACKUP:
+        js.dump(d,BACKUP,indent=2)
+  
+  else :
+    print("wrong command")
